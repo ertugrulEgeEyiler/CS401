a7c3aa577556f029aa9479c66417de515bcce23c
Kaan Alp <kaan.alp@ozu.edu.tr>
2024-03-27 17:58:09 +0300

    added clustering test array.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Hello and welcome!");
+        ArrayList<Integer> toCluster = generateArray();
+        cluster(toCluster);
+    }
+
+    public static ArrayList<Integer> generateArray(){
+        Random random = new Random(1);
+        ArrayList<Integer> array = new ArrayList<Integer>(100);
+        for (int i = 0; i < 100; i++){
+            int number = random.nextInt(2);
+            array.add(i,number);
         }
+
+        return array;
+    }
+
+    public static void cluster(ArrayList<Integer> toCluster){
+
     }
 }
